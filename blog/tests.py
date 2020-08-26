99cd48e5fe649e7b2dabd552502c6c690c8d52a6
Jake Stewart <jxs1170@cs.bham.ac.uk>
2020-08-26 12:43:32 +0100

    Added the start of first testcase

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+class NewVisitorTest(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_open_a_blog_post(self):
+        #Opens the homepage
+        self.browser.get('http://localhost:8000')
+        #Reads the header
+        self.assertIn('Jake\'s Blog', self.browser.title)
+        self.fail('Finish the test!')
+        #Finds a list of blog posts
+        #Selects one to open
+        #Clicks on it
+        #Is directed to the blog post to read it
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost',]
 
 LOGIN_REDIRECT_URL='/'
 
