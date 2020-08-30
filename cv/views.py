99a1f752d08aad75d1dc3c0bfd4e94fc36833847
Jake Stewart <jxs1170@cs.bham.ac.uk>
2020-08-30 15:47:04 +0100

    Added functionality to pass header and title tests

--- cv/views.py
+++ cv/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def cv_page(request):
-    return HttpResponse('\n<html><title>Jake\'s CV</title></html>\n')
+    return render(request, 'cv/cv_page.html')
