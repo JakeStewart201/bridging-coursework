4e006f623c87a4b9b50799621aacc80787be82cf
Jake Stewart <jxs1170@cs.bham.ac.uk>
2020-08-30 15:21:04 +0100

    Fixed test using sing = for equality

--- cv/functional_tests.py
+++ cv/functional_tests.py
@@ -24,35 +24,35 @@ class NewVisitorTest(unittest.TestCase):
         eduction = self.browser.find_element_by_id('education')
         qualifications = education.find_elements_by_class_name('qualification')
         self.assertTrue(
-            any(qualification.find_element_by_class_name('institution').text = 'Univeristy of Birmingham' for qualification in qualifications)
+            any(qualification.find_element_by_class_name('institution').text == 'Univeristy of Birmingham' for qualification in qualifications)
         )
 
         #Can read employment history
         employment_history = self.browser.find_element_by_id('employment history')
         jobs = education.find_elements_by_class_name('employment')
         self.assertTrue(
-            any(job.find_element_by_class_name('employer').text = 'Barnardo\'s Charity Shop' for job in jobs)
+            any(job.find_element_by_class_name('employer').text == 'Barnardo\'s Charity Shop' for job in jobs)
         )
 
         #Can read skills
         skill_list = self.browser.find_element_by_id('skills')
         skills = education.find_elements_by_class_name('skill')
         self.assertTrue(
-            any(skill.find_element_by_class_name('name').text = 'Python' for skill in skills)
+            any(skill.find_element_by_class_name('name').text == 'Python' for skill in skills)
         )
 
         #Can read interests
         interest_list = self.browser.find_element_by_id('interests')
         interests = education.find_elements_by_class_name('interest')
         self.assertTrue(
-            any(interest.find_element_by_class_name('name').text = 'Climbing' for interest in interests)
+            any(interest.find_element_by_class_name('name').text == 'Climbing' for interest in interests)
         )
 
         #Can read projects
         project_list = self.browser.find_element_by_id('projects')
         projects = education.find_elements_by_class_name('project')
         self.assertTrue(
-            any(project.find_element_by_class_name('name').text = 'Quacker Royale' for project in projects)
+            any(project.find_element_by_class_name('name').text == 'Quacker Royale' for project in projects)
         )
 
 
