e117b1c7fc0042cafa127c3f8c9ebd212cdb099a
Jake Stewart <jxs1170@cs.bham.ac.uk>
2020-08-30 14:49:39 +0100

    Created a more extensive functional test for the cv page

--- cv/functional_tests.py
+++ cv/functional_tests.py
@@ -9,14 +9,52 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_open_a_blog_post(self):
+    def test_can_read_cv(self):
+        #Opens cv
         self.browser.get('http://localhost:8000/cv')
 
+        #Sees that it's the cv in the title
         self.assertIn('Jake\'s CV', self.browser.title)
+
+        #Sees the title for the CV
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Jake\'s CV', header_text)
 
-        self.fail('Finish rest of test')
+        #Can read education history
+        eduction = self.browser.find_element_by_id('education')
+        qualifications = education.find_elements_by_class_name('qualification')
+        self.assertTrue(
+            any(qualification.find_element_by_class_name('institution').text = 'Univeristy of Birmingham' for qualification in qualifications)
+        )
+
+        #Can read employment history
+        employment_history = self.browser.find_element_by_id('employment history')
+        jobs = education.find_elements_by_class_name('employment')
+        self.assertTrue(
+            any(job.find_element_by_class_name('employer').text = 'Barnardo\'s Charity Shop' for job in jobs)
+        )
+
+        #Can read skills
+        skill_list = self.browser.find_element_by_id('skills')
+        skills = education.find_elements_by_class_name('skill')
+        self.assertTrue(
+            any(skill.find_element_by_class_name('name').text = 'Python' for skill in skills)
+        )
+
+        #Can read interests
+        interest_list = self.browser.find_element_by_id('interests')
+        interests = education.find_elements_by_class_name('interest')
+        self.assertTrue(
+            any(interest.find_element_by_class_name('name').text = 'Climbing' for interest in interests)
+        )
+
+        #Can read projects
+        project_list = self.browser.find_element_by_id('projects')
+        projects = education.find_elements_by_class_name('project')
+        self.assertTrue(
+            any(project.find_element_by_class_name('name').text = 'Quacker Royale' for project in projects)
+        )
+
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
