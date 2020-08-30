b1d43c6e41045f2aac85fa65f5407cec0404ec9a
Jake Stewart <jxs1170@cs.bham.ac.uk>
2020-08-30 13:57:54 +0100

    CV page now passes first test to resove address

--- cv/tests.py
+++ cv/tests.py
@@ -2,17 +2,17 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
-from blog.views import cv
+from cv.views import cv_page
 
 class CVPageTest(TestCase):
 
     def test_cv_url_resolves_to_cv_view(self):
         found = resolve('/cv')
-        self.assertEquals(found.func,  cv)
+        self.assertEquals(found.func,  cv_page)
 
     def test_cv_returns_correct_html(self):
         request = HttpRequest()
-        response = post_list(request)
+        response = cv_page(request)
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('\n<html>'))
         self.assertIn('<title>Jake\'s CV</title>', html)
