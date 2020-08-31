40b140c602dc7e56b91eaeedc4f738aebf97d01a
Jake Stewart <jxs1170@cs.bham.ac.uk>
2020-08-31 19:04:20 +0100

    Added extra unit tests that do the same as intergration tests

--- cv/tests.py
+++ cv/tests.py
@@ -1,10 +1,15 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.contrib.auth import get_user_model
 
 from cv.views import cv_page
 
 class CVPageTest(TestCase):
+    
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create_user('temp', 'temp@gmail.com', 'temp')
 
     def test_cv_url_resolves_to_cv_view(self):
         found = resolve('/cv/')
@@ -17,3 +22,26 @@ class CVPageTest(TestCase):
         self.assertTrue(html.startswith('\n<html>'))
         self.assertIn('<title>Jake\'s CV</title>', html)
         self.assertTrue(html.endswith('</html>\n'))
+
+    def test_cv_uses_cv_template(self):
+        response = self.client.get('/cv/')
+        self.assertTemplateUsed(response, 'cv/cv_page.html')
+
+    def test_can_save_an_interest_POST_request(self):
+        User = get_user_model()
+        self.client.login(username='temp', password='temp')
+
+        response = self.client.post('/edit_interest/', data={'name':'test','importance':'100','text':'test text'}, follow=True)
+
+        self.assertIn('test text', response.content.decode())
+        self.assertTemplateUsed(response, 'cv/cv_page.html')
+
+
+    def test_can_redirect_an_interest_POST_request(self):
+        User = get_user_model()
+        self.client.login(username='temp', password='temp')
+
+        response = self.client.post('/edit_interest/', data={'name':'test','importance':'100','text':'test text'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/cv/')
